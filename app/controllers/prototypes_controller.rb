e1a23a1e3e5b89e8b635d7d8b8ed8dcd51a3d88a
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 21:46:32 +0900

    データの保存　分岐の記述

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,12 +8,17 @@ def new
 end
 
 def create
-  Prototype.create（prototype_params)
+  @prototype = Prototype.new(prototype_params)
+  if @prototype.save # ifの隣も実行するされる、saveメソッドがSQLの実行だけでなく、保存が出来たらture, バリデーションなどにより失敗したらfalseを返すため
+    redirect_to loot_path # pathのつけ忘れに注意
+  else
+    render 'new' 
+  end
 end
 
 private
-def prototype_params # ストロングパラメーター設定　user_idの結合
-  parmas.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+def prototype_params # ストロングパラメーター設定,user_idの結合
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
 
 end
