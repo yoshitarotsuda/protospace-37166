0c95897218698bfb276c4de9981dd421b4bea019
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 17:03:15 +0900

    投稿機能のモデルにアソシエーション記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,2 +1,10 @@
 class Prototype < ApplicationRecord
+
+  # with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
+  #   validates :
+  #   validates :
+  #   validates :
+  # end
+
+  belongs_to :user # 相手のモデルのidが空ではないかというバリデーションがデフォルトでかかる。
 end
