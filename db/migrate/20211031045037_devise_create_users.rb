570b175284aa217dfeda461eb8a7a68d25597be6
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 14:07:32 +0900

    マイグレーションファイル記述　マイグレート

--- db/migrate/20211031045037_devise_create_users.rb
+++ db/migrate/20211031045037_devise_create_users.rb
@@ -6,6 +6,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
+      t.text   :profile,            null: false
+      t.text   :occupation,         null: false
+      t.text   :position,           null: false
+
 
       ## Recoverable
       t.string   :reset_password_token
@@ -36,7 +41,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email,                unique: true   # ユニーク制約を記述(DB側) アプリケーション側にも設定予定
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
