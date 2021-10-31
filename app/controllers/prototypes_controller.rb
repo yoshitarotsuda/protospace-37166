ff0527f44b00b513e5f6703a2d38c9320929e096
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 19:59:09 +0900

    ストロングパラメーターの設定　user_idの結合

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,6 +8,12 @@ def new
 end
 
 def create
+  Prototype.create（prototype_params)
+end
+
+private
+def prototype_params # ストロングパラメーター設定　user_idの結合
+  parmas.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
 
 end
