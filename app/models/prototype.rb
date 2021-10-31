c917fd0b68fc77e8bcfe3717620bc14f9e93e7d0
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 19:20:12 +0900

    バリデーションの記述

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,10 +1,12 @@
 class Prototype < ApplicationRecord
 
-  # with_options presence: true do 
-  #   validates :title
-  #   validates :catch_copy
-  #   validates :concept
-  # end
+  with_options presence: true do 
+    validates :title
+    validates :catch_copy
+    validates :concept
+    validates :image
+
+  end
 
   belongs_to :user # 相手のモデルのidが空ではないかというバリデーションがデフォルトでかかる。
   has_one_attached :image # レコードとファイルの1対1の関係の紐付け
