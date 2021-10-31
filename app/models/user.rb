804d8973d5a2d8355a777384670f26f8b9f93032
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-10-31 14:31:07 +0900

    バリデーションの設定
    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, uniqueness: true # ユニーク制約(アプリケーション側)
+
+  with_options presence: true do #with_options オプションの記述をまとめるのに使える、if使用時の記述パターンに注意
+    validates :name
+    validates :profile
+    validates :occupation
+    validates :position
+  end
+
+
 end
+
+
+
+
+# validatesメモ
+# 
