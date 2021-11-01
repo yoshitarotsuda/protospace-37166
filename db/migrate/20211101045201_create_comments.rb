497117c496665443d69c30035d6122ee0e9fbf12
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-01 14:00:03 +0900

    マイグレーションファイルの記入　マイグレートの実行

--- db/migrate/20211101045201_create_comments.rb
+++ db/migrate/20211101045201_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-
+      t.text       :content,   null: false
+      t.references :user,      null: false, foreign_key: true # 外部キー設定
+      t.references :prototype, null: false, foreign_key: true # 外部キー設定
       t.timestamps
     end
   end
