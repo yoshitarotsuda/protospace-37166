92438b349fda8e27a10817971706e854c3307629
yoshitarotsuda <tsuda.shinden@gmail.com>
2021-11-01 14:38:42 +0900

    バリデーションの設定

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  # validates :content presence: true
+  validates :content, presence: true
 
   belongs_to :user # 相手のモデルのidが空ではないかというバリデーションがデフォルトでかかる。
   belongs_to :prototype
